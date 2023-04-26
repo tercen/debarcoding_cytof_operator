a6b5976310bf6f451e3de9b1fd204503452b9c28
Thiago <tsmonteiro@gmail.com>
2023-04-26 00:28:32 +0000

    jsonlite

--- main.R
+++ main.R
@@ -78,7 +78,7 @@ if(is.null(ctx$task)) {
   stop("task is null")
 } else {
   pair <- Find(function(pair) identical(pair$key, "task.siblings.id"), ctx$task$environment)
-  task_siblings_id <- jsonlite::fromJSON(pair$value)
+  task_siblings_id <- jsonlite::fromJSON(pair$value) 
   ctx2 <- tercenCtx(taskId = task_siblings_id)
 }
 
